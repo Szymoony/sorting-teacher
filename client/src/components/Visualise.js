41e75cafe74b541b8e9f208021c2f9f3f71f3d9e
plzfday <plzfday@gmail.com>
2022-03-24 17:32:02 +0000

    Change Visualise into a function component

--- client/src/components/Visualise.js
+++ client/src/components/Visualise.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 import { Container, Row, Button, ProgressBar, Col } from 'react-bootstrap';
 import Bar from './Bar';
 import History from './History';
@@ -6,11 +6,14 @@ import { sortableContainer, sortableElement } from 'react-sortable-hoc';
 import { arrayMoveImmutable } from 'array-move';
 import { bubbleSort, insertionSort, selectionSort } from '../assets/Sorting';
 
-const SortableItem = sortableElement(({ value, _height, _width }) => (
-  <Bar height={_height} width={_width}>
-    {value}
-  </Bar>
-));
+const SortableItem = sortableElement(({ value, _width }) => {
+  const height = `${Math.min(400, 30 + (value - 1) * 5)}px`;
+  return (
+    <Bar height={height} width={_width}>
+      {value}
+    </Bar>
+  );
+});
 
 const SortableContainer = sortableContainer(({ children }) => (
   <Row
@@ -24,67 +27,13 @@ const SortableContainer = sortableContainer(({ children }) => (
   </Row>
 ));
 
-class Visualise extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: props.list.slice(),
-      currentStep: 1,
-    };
-    this.steps = this.updateSteps();
-  }
+const Visualise = (props) => {
+  const [items, setItems] = useState(props.list.slice());
+  const [currentStep, setCurrentStep] = useState(1);
 
-  onSortEnd = ({ oldIndex, newIndex }) => {
-    this.setState(({ items }) => ({
-      items: arrayMoveImmutable(items, oldIndex, newIndex),
-    }));
-  };
-
-  createBars() {
-    const { items } = this.state;
-    const percentage = (((this.state.currentStep - 1) / (this.steps.length - 1)) * 100).toFixed(1);
-    return (
-      <Container>
-        <Row>
-          <SortableContainer
-            children={items.map((value, index) => (
-              <SortableItem key={`item-${index}`} index={index} value={value} _height={`${Math.min(400, 30 + (value - 1) * 5)}px`} _width={'40px'} />
-            ))}
-            axis='x'
-            onSortEnd={this.onSortEnd}
-          ></SortableContainer>
-        </Row>
-        <Row className='justify-content-md-center mt-3'>
-          <Col lg='7'>
-            <ProgressBar now={percentage} label={`${percentage}%`} />
-          </Col>
-          <Col lg='1'>
-            <Button variant='outline-dark' onClick={this.validate.bind(this)}>
-              Next
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-
-  render() {
-    return (
-      <Row>
-        <Col md={8}>{this.createBars()}</Col>
-        <Col md={4}>
-          <Container className='my-3'>
-            <h1 style={{ fontSize: '20px' }}>History ({this.props.algoType.charAt(0).toUpperCase() + this.props.algoType.slice(1) + ' Sort'})</h1>
-            <History steps={this.steps} id={this.state.currentStep} />
-          </Container>
-        </Col>
-      </Row>
-    );
-  }
-
-  updateSteps() {
-    const type = this.props.algoType;
-    const copiedList = this.props.list.slice();
+  const getSteps = () => {
+    const type = props.algoType;
+    const copiedList = props.list.slice();
     if (type === 'bubble') {
       return bubbleSort(copiedList);
     } else if (type === 'insertion') {
@@ -92,25 +41,67 @@ class Visualise extends Component {
     } else if (type === 'selection') {
       return selectionSort(copiedList);
     }
-  }
+  };
 
-  validate() {
-    const items = this.state.items;
-    const count = this.state.currentStep;
-    if (this.steps.length === this.state.currentStep) {
+  const steps = useRef(getSteps());
+
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setItems(arrayMoveImmutable(items, oldIndex, newIndex));
+  };
+
+  const createBars = () => {
+    const percentage = (((currentStep - 1) / (steps.length - 1)) * 100).toFixed(1);
+    return (
+      <Container>
+        <Row>
+          <SortableContainer
+            children={items.map((value, index) => (
+              <SortableItem key={`item-${index}`} index={index} value={value} _width='40px' />
+            ))}
+            axis='x'
+            onSortEnd={onSortEnd}
+          ></SortableContainer>
+        </Row>
+        <Row className='justify-content-md-center mt-3'>
+          <Col lg='7'>
+            <ProgressBar now={percentage} label={`${percentage}%`} />
+          </Col>
+          <Col lg='1'>
+            <Button variant='outline-dark' onClick={validate}>
+              Next
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+
+  const validate = async () => {
+    if (steps.length === currentStep) {
       alert('Done');
       return;
     }
-    if (JSON.stringify(items) === JSON.stringify(this.steps[count])) {
-      this.setState({ currentStep: this.state.currentStep + 1 }, () => {
-        if (this.steps.length === this.state.currentStep) {
-          alert('Done');
-        }
-      });
+    if (JSON.stringify(items) === JSON.stringify(steps[currentStep])) {
+      await setCurrentStep(currentStep + 1);
+      if (steps.length === currentStep) {
+        alert('Done');
+      }
     } else {
       alert('Wrong Relocation!');
     }
-  }
-}
+  };
+
+  return (
+    <Row>
+      <Col md={8}>{createBars()}</Col>
+      <Col md={4}>
+        <Container className='my-3'>
+          <h1 style={{ fontSize: '20px' }}>History ({props.algoType.charAt(0).toUpperCase() + props.algoType.slice(1) + ' Sort'})</h1>
+          <History steps={steps} id={currentStep} />
+        </Container>
+      </Col>
+    </Row>
+  );
+};
 
 export default Visualise;
